335187dc44abba6ba200f4f60ccb739beeb54789
Andrew Dassonville <dassonville.andrew@gmail.com>
2018-09-08 16:16:21 -0700

    clean stuff up a bit

--- travis/travis_docker.sh
+++ travis/travis_docker.sh
@@ -3,4 +3,4 @@ set -e   # Make sure any error makes the script to return an error code
 
 export STAGE_INPUT_IMAGE=sert/dunk:latest
 
-docker run -v $HOME:$HOME $CODECOV_VARS -e CC -e BUILD_TARGET -e STAGE -e BUILD_TYPE -e MAKEFLAGS -e CCACHE_SLOPPINESS -e TASK -e DEPS -e CI_SOURCE_PATH -e REPOSITORY_NAME -e HOME -e DISTRO -e CI_ROS_DISTRO -e CODECOV_TOKEN --name mrptbuild $STAGE_INPUT_IMAGE bash -c 'cd $CI_SOURCE_PATH; apt-get update -qq; source travis/travis_main.sh'
+docker run -v $HOME:$HOME $CODECOV_VARS -e CC -e BUILD_TARGET -e STAGE -e BUILD_TYPE -e MAKEFLAGS -e CCACHE_SLOPPINESS -e TASK -e DEPS -e CI_SOURCE_PATH -e REPOSITORY_NAME -e HOME -e CODECOV_TOKEN --name dunkbuild $STAGE_INPUT_IMAGE bash -c 'cd $CI_SOURCE_PATH; apt-get update -qq; source travis/travis_main.sh'
