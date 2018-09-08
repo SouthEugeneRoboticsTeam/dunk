b11bec23bce07ce289115e3d294b6f213fdc33c1
Andrew Dassonville <dassonville.andrew@gmail.com>
2018-09-08 15:57:33 -0700

    oh my god im in physical pain

--- travis/travis_docker.sh
+++ travis/travis_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e   # Make sure any error makes the script to return an error code
 
-export STAGE_INPUT_IMAGE=sert/dunk
+export STAGE_INPUT_IMAGE=sert/dunk:latest
 
 docker run -v $HOME:$HOME $CODECOV_VARS -e CC -e BUILD_TARGET -e STAGE -e BUILD_TYPE -e MAKEFLAGS -e CCACHE_SLOPPINESS -e TASK -e DEPS -e CI_SOURCE_PATH -e REPOSITORY_NAME -e HOME -e DISTRO -e CI_ROS_DISTRO -e CODECOV_TOKEN --name mrptbuild $STAGE_INPUT_IMAGE bash -c 'cd $CI_SOURCE_PATH; apt-get update -qq; source travis/travis_main.sh'
