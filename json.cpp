c0dc443ba90a0b3d8eff5c546c8a74ea0de56ba0
Andrew Dassonville <dassonville.andrew@gmail.com>
2018-09-09 13:41:37 -0700

    Add message type to JSON

--- json.cpp
+++ json.cpp
@@ -2,7 +2,8 @@
 
 string json_message(double x, double y, double angle) {
   string json = "{";
-  json += string( "\"x\":") + to_string(x);
+  json += string(" \"type\":\"slam\"");
+  json += string(",\"x\":") + to_string(x);
   json += string(",\"y\":") + to_string(y);
   json += string(",\"angle\":") + to_string(angle);
   json += "}";
