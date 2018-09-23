377e92d36fd7064616b19db90c48e74ef73ff019
Jackson Lewis <st.japa6@gmail.com>
2018-09-22 18:45:00 -0700

    Added epoch_s and epoch_ms values to JSON message

--- json.cpp
+++ json.cpp
@@ -1,8 +1,15 @@
 #include "json.h"
 
 string json_message(double x, double y, double angle) {
+  // Get time
+  struct timeval tp;
+  gettimeofday(&tp, NULL);
+  long int epoch_s = tp.tv_sec;
+  long int epoch_ms = tp.tv_usec * 1000;  
   string json = "{";
-  json += string(" \"type\":\"slam\"");
+  json += string(" \"epoch_s\": ") + to_string(epoch_s);
+  json += string(",\"epoch_ms\": ") + to_string(epoch_ms);
+  json += string(",\"type\":\"slam\"");
   json += string(",\"x\":") + to_string(x);
   json += string(",\"y\":") + to_string(y);
   json += string(",\"angle\":") + to_string(angle);
--- json.h
+++ json.h
@@ -2,6 +2,7 @@
 #define DUNK_JSON_H
 
 #include <string>
+#include <sys/time.h>
 using namespace std;
 
 string json_message(double x, double y, double angle);
